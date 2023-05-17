4599923b73a9313c799eec31d8299ff0e8a3d87a
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-05-17 08:57:22 +0200

    save population as pickle object

---
+++ src/animation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import pickle
+from os.path import join
+from config import Config
+from core.population import Population
+
+CONFIG_FILENAME = "config.yaml"
+OUTPUT_DIRECTORY = "results"
+
+if __name__ == "__main__":
+  with open(CONFIG_FILENAME, "r") as f:
+    Config.loadYaml(f)
+
+  pop = Population()
+  for generation, epoch in pop.generations():
+    print(generation)
+    with open(join(OUTPUT_DIRECTORY, f"epoch_{epoch}.pkl"), "wb") as f:
+      pickle.dump(generation, f)
